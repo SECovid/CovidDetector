c4ec3b1b3822b3d3d634beb594c109abe24e00a0
charbel <charbel-boumaroun@outlook.com>
2021-04-03 15:30:59 +0300

    feat: Filling placeholders in retraining pipeline

--- ml/model/log.py
+++ ml/model/log.py
@@ -2,18 +2,29 @@ import json
 import datetime as dt
 def modelLogs(history,size):
 
-    today = dt.date.today()
-    d1 = today.strftime("%d-%m-%Y")
+    currentTime = dt.datetime.now()
+    fileDate = currentTime.strftime("%d-%m-%Y-%H-%M-%S")
 
     data = {}
-    data['date'] =  d1
+    data['date'] =  fileDate
     data['training_size'] = size
     data['accuracy'] = history.history['accuracy']
     data['loss'] = history.history['loss']
     #Choose a more meaningful name (Maybe more than one training per day)
-    with open('logs/'+d1+'.json', 'w') as outfile:
+    with open('logs/'+fileDate+'.json', 'w') as outfile:
         json.dump(data, outfile)
 
+def getSizeFromLogs():
+    import glob
+    import os
+    #Get latest logs
+    list_of_files = glob.glob('logs/*.json')  # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    print(latest_file)
+
+    with open(latest_file) as json_file:
+        data = json.load(json_file)
+        return data['training_size']
 
 if __name__ == '__main__':
     modelLogs(None,None)
--- ml/model/retraining_model.py
+++ ml/model/retraining_model.py
@@ -1,45 +1,55 @@
 from ml.data import dataset
 import datetime as dt
 import numpy as np
+from ml.model import log
 
-def retraining_pipeline(date = '01/01/1970'):
+
+def retraining_pipeline(date='01/01/1970'):
     # Get data
     data = list(dataset.get_all_data(date))
-    # Check if enough new data
-    print(len(data))
 
-    # Extract previous model's training size
-    '''
-    import json
-    today = dt.date.today()
-    d1 = today.strftime("%d-%m-%Y")
-    with open('logs/'+d1+'.json') as json_file:
-        data = json.load(json_file)
-        for p in data['people']:
-            print('Name: ' + p['name'])
-            print('Website: ' + p['website'])
-            print('From: ' + p['from'])
-            print('')
-    '''
-    #Compare new size with old size and check if enough
+    # Number of training points
+    current_training_size = len(data)
 
-
-    #Transform into two arrays
+    # Transform into two arrays
     labels = np.array([datapoint['label'] for datapoint in data])
-    #Retrieving all spectrograms and reshaping them to numpy arrays
-    spectrograms = np.reshape(np.array([np.frombuffer(datapoint['spectrogram'],dtype=np.float32) for datapoint in data]),(-1,1025,69))
+    # Retrieving all spectrograms and reshaping them to numpy arrays
+    spectrograms = np.reshape(
+        np.array([np.frombuffer(datapoint['spectrogram'], dtype=np.float32) for datapoint in data]), (-1, 1025, 69))
+
+
 
     # Check if balanced
+    negative_recordings = np.count_nonzero(labels)
+    positive_recordings = current_training_size - negative_recordings
+    THRESHOLD = 40
+    if (abs(positive_recordings - negative_recordings) > THRESHOLD):
+        print('Not balanced dataset')
+        minimum_recordings = min(positive_recordings, negative_recordings)
+        #Make it balanced
+        return
+    print('Balanced dataset')
+    #Update current training size
+
+
+
+    # Check if enough data
+    # Extract previous model's training size
+    previous_training_size = log.getSizeFromLogs()
+    # Compare new size with old size and check if enough
+    print('Number of new data: ', (current_training_size - previous_training_size))
+
 
 
 
     # import train model and train the model
     from ml.model import train_model
-    #Reshaping spectrograms for training
+    # Reshaping spectrograms for training
     spectrograms = np.reshape(spectrograms, (spectrograms.shape[0], spectrograms.shape[1], spectrograms.shape[2], 1))
-    train_model.create_model(spectrograms,labels)
+    # train_model.create_model(spectrograms,labels)
     print(spectrograms.shape)
     pass
+
 
 if __name__ == "__main__":
     retraining_pipeline()
