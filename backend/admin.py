4107a41ba61d9458f8fdeb09ca00c1902f318b1e
charbel <charbel-boumaroun@outlook.com>
2021-05-03 12:32:07 +0300

    feat: Added admin and covid report placeholders

---
+++ backend/admin.py
@@ -0,0 +1,12 @@
+#Retrain model manually if admin
+
+#Rollback to previous models
+
+
+#get statistics if admin
+
+#Check model health
+
+#Delete elements from db?
+
+#View How much training data used
---
+++ backend/covid_report.py
@@ -0,0 +1,1 @@
+#Get covid report based on user
